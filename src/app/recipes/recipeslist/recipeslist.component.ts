180b61281d2260ef4d9ff6a06de50cece847d896
Pasumarthy <cpasumarthy@KHECORP.com>
2020-04-04 12:16:29 -0400

    save

--- src/app/recipes/recipeslist/recipeslist.component.ts
+++ src/app/recipes/recipeslist/recipeslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipeslist',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipeslist.component.css']
 })
 export class RecipeslistComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test',
+    'This simply a test',
+    'https://www.simplyrecipes.com/wp-content/uploads/2014/09/Classic-Baked-Chicken-LEAD-4.jpg')
+  ];
 
   constructor() { }
 
