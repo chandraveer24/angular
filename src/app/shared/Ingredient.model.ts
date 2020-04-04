16089db086410261891406d61f739762b1311dec
Pasumarthy <cpasumarthy@KHECORP.com>
2020-04-04 12:17:35 -0400

    first commit

---
+++ src/app/shared/Ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  // Alternatively we can do the following
+  constructor(public name: string, public amount: number) {}
+}
